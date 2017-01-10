2e7ff8da0f173e6f50b8aae96546f7afc2339b8f
Sophie LE BERRE <sophie.slb@live.fr>
2017-01-10 15:44:31 +0100

    rectification

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var seuil= prompt("Combien voulez vous avoir de cases?");
-
+if (seuil<1000){
 for (var i = 1; i <= seuil; i++) {
 	document.getElementById('liste').innerHTML+='<li>'+i+'</li>';
-	
+
+ }}else{
+	alert ('nombre trop élevé');	
 
 }
 
